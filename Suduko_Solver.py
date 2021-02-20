418ccf52747662d023f8729df8db35b00c099622
Kenneth Bectell <Kennethbectell@gmail.com>
2021-02-19 18:43:43 -0800

    Added puzzle parameter to functions

--- Suduko_Solver.py
+++ Suduko_Solver.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # puzzle[row][col]
-puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+grid = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
           [6, 0, 0, 1, 9, 5, 0, 0, 0],
           [0, 9, 8, 0, 0, 0, 0, 6, 0],
           [8, 0, 0, 0, 6, 0, 0, 0, 3],
@@ -12,7 +12,7 @@ puzzle = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
           [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 
 
-def isValid(row: int, col: int, num: int):
+def isValid(puzzle, row, col, num):
     # check row
     for i in range(9):
         if puzzle[i][col] == num:
@@ -33,18 +33,23 @@ def isValid(row: int, col: int, num: int):
     return True
 
 
-def solve():
+def findNextSpace(puzzle):
     for row in range(9):
         for col in range(9):
             if puzzle[row][col] == 0:
-                for num in range(1, 10):
-                    if isValid(row, col, num):
-                        puzzle[row][col] = num
-                        solve()
-                        puzzle[row][col] = 0
-                return
+                return row, col
+
+
+def solve(puzzle):
+    row, col = findNextSpace(puzzle)
+    for num in range(1, 10):
+        if isValid(row, col, num):
+            puzzle[row][col] = num
+            solve(puzzle)
+            puzzle[row][col] = 0
+    return
     print(np.matrix(puzzle))
     input("More?")
 
 
-solve()
+solve(grid)
